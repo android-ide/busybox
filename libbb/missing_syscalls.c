a05a290f7d9fc44640b73e4dc37c90649b685aab
Hans Kratz <hans@appfour.com>
2017-08-01 15:28:02 +0200

    ndk 15 platform 14 does not have personality() function

--- libbb/missing_syscalls.c
+++ libbb/missing_syscalls.c
@@ -39,4 +39,10 @@ int pivot_root(const char *new_root, const char *put_old)
 {
 	return syscall(__NR_pivot_root, new_root, put_old);
 }
+
+int personality(unsigned long persona)
+{
+	return syscall(__NR_personality, persona);
+}
+
 #endif
